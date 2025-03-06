3a5807acb76d632fdeedac9a11325b42f594641f
sergioivanov008 <sergioivanov008@gmail.com>
2025-03-06 07:49:51 +0300

    refactor(widgets): изменены стили
    изменены стили на странице Main

--- src/widgets/main-header/MainHeader.tsx
+++ src/widgets/main-header/MainHeader.tsx
@@ -3,17 +3,19 @@ import s from './MainHeader.module.scss';
 export const MainHeader = () => {
 	return (
 		<div className={s.mainHeader}>
-			<div className={s.mainHeaderSearch}>
-				<input className={s.headerSearch} type="text" placeholder="Search" />
-				<div className={s.searchLogo} />
-			</div>
-			<div className={s.mainHeaderNav}>
-				<div className={s.navBell} />
-				<div className={s.navAvatar}>
-					<div className={s.avatarName}>Sam</div>
-					<div className={s.avatarArrow} />
-					<div className={s.avatarFoto} />
-				</div>
+			<div className={s.mainHeaderWrapper}>
+			  <div className={s.mainHeaderSearch}>
+			  	<input className={s.headerSearch} type="text" placeholder="Search" />
+			  	<div className={s.searchLogo} />
+			  </div>
+			  <div className={s.mainHeaderNav}>
+			  	<div className={s.navBell} />
+			  	<div className={s.navAvatar}>
+			  		<div className={s.avatarName}>Sam</div>
+			  		<div className={s.avatarArrow} />
+			  		<div className={s.avatarFoto} />
+			  	</div>
+			  </div>
 			</div>
 		</div>
 	);
