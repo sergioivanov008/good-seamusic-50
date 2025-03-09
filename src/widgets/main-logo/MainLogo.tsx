70deb256e8bd9bbf0960ca21147d79239a95cdbc
sergioivanov008 <sergioivanov008@gmail.com>
2025-03-09 16:35:37 +0300

    refactor(widgets): рефакторинг виджета MainLogo
    рефакторинг виджета MainLogo

--- src/widgets/main-logo/MainLogo.tsx
+++ src/widgets/main-logo/MainLogo.tsx
@@ -1,5 +1,6 @@
+import { TEXT } from '@/shared/constants/constants';
 import s from './MainLogo.module.scss';
 
 export const MainLogo = () => {
-	return <div className={s.mainLogo}>SeaMusic</div>;
+	return <div className={s.mainLogo}>{TEXT.LogoTitle}</div>;
 };
