8c56d9568cc6fbc69e43a07ce9353a716fde98de
sergioivanov008 <sergioivanov008@gmail.com>
2025-03-15 02:18:33 +0300

    fix(app): исправлена ошибка задваивания Layout
    исправлена ошибка задваивания Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@
 
 import { Montserrat } from 'next/font/google';
 import './globals.css';
-import s from './RootLayout.module.scss';
-import { AudioPlayer, MainHeader, MainLogo, MainMenu } from '@/widgets';
-import { TRACK_ITEMS } from '@/shared/constants/constants';
+// import s from './RootLayout.module.scss';
+// import { AudioPlayer, MainHeader, MainLogo, MainMenu } from '@/widgets';
+// import { TRACK_ITEMS } from '@/shared/constants/constants';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
@@ -16,15 +16,7 @@ const RootLayout = ({
 	return (
 		<html lang="ru">
 			<body className={montserrat.className}>
-				<div className={s.main}>
-					<MainLogo />
-					<MainHeader />
-					<MainMenu />
-					<div className={s.content}>
-						<div className={s.mainContent}>{children}</div>
-					</div>
-					<AudioPlayer tracks={TRACK_ITEMS} />
-				</div>
+			  {children}
 			</body>
 		</html>
 	);
