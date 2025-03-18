b5f878a2b43f6bc747149a5e5ce341d2d939cc73
sergioivanov008 <sergioivanov008@gmail.com>
2025-03-19 00:50:58 +0300

    feat(entities): добавлен энтитис Role
    добавлен энтитис Role

---
+++ src/entities/role/Role.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { RoleItem } from '@/shared/ui';
+import s from './Role.module.scss';
+import { TEXT } from '@/shared/constants/constants';
+
+type RoleType = {
+	id: string;
+	name: string;
+	role: string;
+};
+
+type RoleProps = {
+	role: RoleType[];
+	handler: (role: string) => void;
+	userRole: string;
+};
+
+export function Role({ role, handler, userRole }: RoleProps) {
+	return (
+    <div className={s.roleWrapper}>
+			<div className={s.roleTitle}>{TEXT.WhoAreYou}</div>
+			<div className={s.role}>
+				{role.map((el) => (
+					<RoleItem key={el.id} name={el.name} role={el.role} handler={handler} userRole={userRole} />
+				))}
+			</div>
+		</div>
+	);
+}
