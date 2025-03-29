2c77938fe369b0dd38d8534020225699e7cd48cb
sergioivanov008 <sergioivanov008@gmail.com>
2025-03-29 20:19:43 +0300

    feat(app): настроен redux/toolkit
    настроен redux/toolkit

--- providers.tsx
+++ providers.tsx
@@ -1,7 +1,15 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-	return <SessionProvider>{children}</SessionProvider>;
+	return (
+	  <Provider store={store}>
+			<SessionProvider>
+				{children}
+			</SessionProvider>
+		</Provider>
+	);
 };
---
+++ store/slices/userSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+interface MainState {
+  id: string;
+}
+
+const initialState: MainState = {
+  id: '',
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setId(state, action: PayloadAction<string>) {
+      state.id = action.payload;
+    },
+  },
+})
+
+export const { reducer: userReducer, actions: mainActions } = userSlice;
+
+export const mainSelector = (state: RootState) => state.user;
